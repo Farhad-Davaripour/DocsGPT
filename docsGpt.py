9c368b9df3580dfb35cbf2bbc1a4f9564a846c8e
Armin <arminnorouzi2016@gmail.com>
2023-04-15 15:27:24 -0400

    feat: run_query updates and change functions of python file

--- docsGpt.py
+++ docsGpt.py
@@ -24,21 +24,21 @@ for name in library_names:
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', name])
 
 
-
+# Import required modules
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-
-# print("You need OpenAI token: Here is the link to get the keys: https://platform.openai.com/account/billing/overview")
-
 from getpass import getpass
+import os
+
+# adding token
+# print("You need OpenAI token: Here is the link to get 
+# the keys: https://platform.openai.com/account/billing/overview")
 
 token = getpass("Enter your OpenAI token: ()")
-
-import os
 os.environ["OPENAI_API_KEY"] = str(token)
 
 
@@ -48,7 +48,20 @@ chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
 
 def upload_doc(reader):
+    """
+    Extracts text from a PDFReader object and splits it into 
+    chunks using the CharacterTextSplitter class. 
+    Computes embeddings for each chunk using an embeddings 
+    model and indexes them using the FAISS library. 
 
+    Args:
+    - reader: A PDFReader object containing the PDF file to 
+    be processed.
+
+    Returns:
+    - A FAISS index object containing the embeddings of the 
+    text chunks.
+    """
     raw_text = ''
     for i, page in enumerate(reader.pages):
         text = page.extract_text()
@@ -56,13 +69,12 @@ def upload_doc(reader):
             raw_text += text
 
     # retreival we don't hit the token size limits. 
-
     text_splitter = CharacterTextSplitter(        
-        separator = "\n",
-        chunk_size = 1000,
-        chunk_overlap  = 200,
-        length_function = len,
-    )
+                                            separator = "\n",
+                                            chunk_size = 1000,
+                                            chunk_overlap  = 200,
+                                            length_function = len,
+                                        )
 
     texts = text_splitter.split_text(raw_text)
 
@@ -72,36 +84,72 @@ def upload_doc(reader):
 
 
 def run_query(query, file):
+    """
+    Runs a query on a PDF file using the docsearch and chain 
+    libraries. 
 
+    Args:
+    - query: A string representing the query to be run.
+    - file: A PDFReader object containing the PDF file to be 
+    searched.
+
+    Returns:
+    - A string containing the output of the chain library run 
+    on the documents returned by the docsearch similarity search.
+    """
+        
     docsearch = upload_doc(file)
     docs = docsearch.similarity_search(query)
     return chain.run(input_documents=docs, question=query)
 
 
 def upload_file(folder_path):
-  uploaded = files.upload()
+    """
+    Uploads a file from the local file system and saves it to 
+    a folder path. 
 
-  for filename, data in uploaded.items():
-      with open(filename, 'wb') as f:
-          f.write(data)
-      shutil.copy(filename, folder_path + "/")
-      root_file = folder_path + "/" + filename
-      os.remove(filename)
+    Args:
+    - folder_path: A string representing the folder path where 
+    the file will be saved.
 
-  return root_file
+    Returns:
+    - A string representing the path of the uploaded file.
+    """
+    
+    uploaded = files.upload()
 
+    for filename, data in uploaded.items():
+        with open(filename, 'wb') as f:
+            f.write(data)
+        shutil.copy(filename, folder_path + "/")
+        root_file = folder_path + "/" + filename
+        os.remove(filename)
+
+    return root_file
 
 
 def run_conversation(folder_path):
-  # location of the pdf file/files.
-  reader = PdfReader(str(upload_file(folder_path)))
+    """
+    Initiates a conversation with the user by repeatedly asking for 
+    input queries and running them on a PDF file. 
 
-  count = 0
-  while True:
-      print("Question ", count + 1)
-      query = input(" Ask your question: ")
-      print(run_query(query, reader))
-      count += 1
-      if count == 10:
-          print("### You asked 10 questions, run cell to ask more questions! ###")
-          break
+    Args:
+    - folder_path: A string representing the folder path where the 
+    PDF file is located.
+
+    Returns:
+    - Run conversation based on PDF
+    """
+      
+    # location of the pdf file/files.
+    reader = PdfReader(str(upload_file(folder_path)))
+
+    count = 0
+    while True:
+        print("Question ", count + 1)
+        query = input(" Ask your question: ")
+        print(run_query(query, reader))
+        count += 1
+        if count == 10:
+            print("### You asked 10 questions, run cell to ask more questions! ###")
+            break
